495fabf5d24e2efe9a6f51febab6f3319a9e9ba9
gnwl <larkpan@protonmail.com>
2023-09-06 13:35:39 -0500

    move auras into core

--- core.lua
+++ core.lua
@@ -243,6 +243,65 @@ function NotGrid:SetIncHealFrame(f, healamount, currhealth, maxhealth) -- well t
 	f.incheal:SetBackdropColor(color[1],color[2],color[3],color[4]) -- instead of hide/show I set opacity. Note that I can't use SetAlpha cause it will hide the child elements
 end
 
+---------------
+-- UNIT_AURA --
+---------------
+
+function NotGrid:UNIT_AURA(unitid)
+	local o = self.o
+	local f = self.UnitFrames[unitid]
+
+	local bufftable = {} -- I only care about buffname for buffs -- reset every time
+	local debufftable = {} -- spelltype only matters for debuffs -- could probably have these both in the same table
+
+	if f and UnitExists(unitid) then
+		--get buff info -- loop through every buff and adds info to table
+		local bi = 1
+		while (UnitBuff(unitid,bi) ~= nil) do
+			self.Gratuity:SetUnitBuff(unitid,bi)
+			local buffname = self.Gratuity:GetLine(1)
+			if buffname then
+				bufftable[buffname] = true
+			end
+			bi = bi + 1;
+		end
+
+		--get debuff info -- same as above
+		local di = 1
+		while (UnitDebuff(unitid,di) ~= nil) do
+			self.Gratuity:SetUnitDebuff(unitid,di)
+			local debuffname = self.Gratuity:GetLine(1)
+			local _, _, spelltype =  UnitDebuff(unitid,di) -- texture, applications, type
+			if debuffname then
+				debufftable[debuffname] = true
+			end
+			if spelltype then
+				debufftable[spelltype] = true
+			end
+			di = di + 1;
+		end
+
+		for i=1,8 do
+			local f = f.healthbar["trackingicon"..i]
+			if self:CheckAura(o["trackingicon"..i],bufftable,debufftable) then
+				f:Show()
+			else
+				f:Hide()
+			end
+		end
+	end
+end
+
+function NotGrid:CheckAura(str, bufftable, debufftable)
+	if str then
+		for text in string.gfind(str, "([^|]+)") do
+			if bufftable[text] or debufftable[text] then
+				return true
+			end
+		end
+	end
+end
+
 -----------------
 -- UNIT_BORDER --
 -----------------
